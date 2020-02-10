c7d254dfb3460c62c80c45692459521571153701
skumargit76 <s.kumar.mcs@gmail.com>
2020-02-11 07:00:01 +0800

    myfirstcommit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorldGit.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            int i = 0;
+            for (int j = 0; j < 10; j++)
+            { }
+                return View();//this
+        }
+    }
+}
